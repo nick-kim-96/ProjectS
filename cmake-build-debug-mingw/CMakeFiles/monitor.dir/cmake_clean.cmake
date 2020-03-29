588238c0125818149482998e4f61b1b40d8e8a03
Sam <SK-Sam@users.noreply.github.com>
2020-03-29 03:47:43 -0400

    Separated to sam_branch
    .

---
+++ cmake-build-debug-mingw/CMakeFiles/monitor.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monitor.dir/src/format.cpp.obj"
+  "CMakeFiles/monitor.dir/src/linux_parser.cpp.obj"
+  "CMakeFiles/monitor.dir/src/main.cpp.obj"
+  "CMakeFiles/monitor.dir/src/ncurses_display.cpp.obj"
+  "CMakeFiles/monitor.dir/src/process.cpp.obj"
+  "CMakeFiles/monitor.dir/src/processor.cpp.obj"
+  "CMakeFiles/monitor.dir/src/system.cpp.obj"
+  "libmonitor.dll.a"
+  "monitor.exe"
+  "monitor.exe.manifest"
+  "monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
